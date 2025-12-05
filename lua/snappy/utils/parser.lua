7946ef3431b9ec21ba46764e0115613f22e8e726
NeoSahadeo <neosahadeo@protonmail.com>
2025-12-05 10:01:29 +0200

    [bug] I've been trying to use api.nvim_buf_get_extmarks but I believe it's bugged. It does not return anything, even when inspect the mouse cursor pos.
    This might be a range issue but I'm probably going to abandon it.

--- lua/snappy/utils/parser.lua
+++ lua/snappy/utils/parser.lua
@@ -3,7 +3,10 @@ local misc = require("snappy.utils.misc")
 local colors = require("snappy.utils.colors")
 
 local M = {
+  lang = nil,
   current_buffer = nil,
+  parser = nil,
+  fallback_fg = require("snappy.utils.colors"):get_fg(),
 }
 
 ---@return string
@@ -18,45 +21,98 @@ local function plain_parse()
 end
 
 local function traverse_nodes(node, callback)
-  if node:child_count() == 0 then
-    print(node:type())
-    callback(node)
-  end
+  callback(node)
+  -- if node:child_count() == 0 then
+  --   print(node:type())
+  -- end
   for child in node:iter_children() do
     traverse_nodes(child, callback)
   end
+end
+
+local function get_hl_group(bufnr, node)
+  if M.lang == nil then
+    return
+  end
+  local hl = vim.treesitter.highlighter.new(M.parser)
+  -- local ns = vim.api.nvim_get_namespaces()["nvim.treesitter.highlighter"]
+  local ns = vim.api.nvim_create_namespace("nvim.treesitter.highlighter")
+  local row1, col1, row2, col2 = node:range()
+
+  -- print(ns)
+  print(vim.inspect(vim.api.nvim_get_namespaces())) -- Find "nvim.treesitter.highlighter"
+  local links = vim.api.nvim_buf_get_extmarks(M.current_buffer, ns, { row1, col1 }, { row2, col2 }, {
+    type = "highlight",
+  })
+  print(links[1])
+  -- for index, value in ipairs(links) do
+  --   print(index)
+  -- end
+  -- if links[1] then
+  --   local hl_id = links[1][4].hl_group
+  --   if hl_id then
+  --     return vim.fn.synIDattr(vim.fn.hlID(hl_id), "name")
+  --   end
+  -- end
+end
+
+local function find_color(capture_name)
+  local command = "highlight @"
+  local color = M.fallback_fg
+  local color_raw = ""
+  while true do
+    local ok = pcall(function()
+      color_raw = vim.api.nvim_exec2(command .. capture_name, { output = true }).output:match("([^%s]+)$")
+    end)
+    if color_raw == nil then
+      break
+    end
+    if not ok then
+      break
+    end
+
+    if string.find(color_raw, "#") == nil then
+      -- print(color_raw)
+      command = "highlight "
+      capture_name = color_raw
+    else
+      color = color_raw:match("#%x+")
+      break
+    end
+  end
+  return color
 end
 
 ---@return (nil|string)
 function M.parse()
   M.current_buffer = vim.api.nvim_get_current_buf()
 
-  local lang = nil
+  M.lang = nil
   local lang_ok = pcall(function()
-    lang = vim.treesitter.language.get_lang(vim.bo.filetype) or vim.bo.filetype
+    M.lang = vim.treesitter.language.get_lang(vim.bo.filetype) or vim.bo.filetype
   end)
-  if not lang_ok or lang == nil then
+  if not lang_ok or M.lang == nil then
     vim.lsp.log.error("Language undetermined")
     return plain_parse()
   end
 
-  local parser = nil
+  M.parser = nil
   local parser_ok = pcall(function()
-    parser = vim.treesitter.get_parser(M.current_buffer, lang)
+    M.parser = vim.treesitter.get_parser(M.current_buffer, M.lang)
   end)
-  if not parser_ok or parser == nil then
+  if not parser_ok or M.parser == nil then
     vim.lsp.log.error("Failed to obtain parser")
     return nil
   end
 
-  local query = vim.treesitter.query.get(lang, "highlights")
+  local query = vim.treesitter.query.get(M.lang, "highlights")
   if query == nil then
     vim.lsp.log.error("No query found")
     return nil
   end
 
   local range = misc.get_visual_selection_range()
-  local tree = parser:parse(range)[1]
+  local tree = M.parser:parse(range)[1]
   local root = tree:root()
 
   local fallback_fg = require("snappy.utils.colors"):get_fg()
@@ -67,94 +123,53 @@ function M.parse()
   local prev_end = 0
   local check_tabs = true
 
+  local marks = {}
+
+  -- local ns = vim.api.nvim_get_namespaces()["nvim.treesitter.highlighter"]
+  local ns = vim.api.nvim_get_namespaces()["NvimTreeHighlights"]
+
+  -- print(ns)
+  -- print(vim.inspect(vim.api.nvim_get_namespaces()))
+
+  -- ---@param node TSNode
   traverse_nodes(root, function(node)
-    local text = vim.treesitter.get_node_text(node, M.current_buffer)
-    for id, match_node in query:iter_captures(node, M.current_buffer) do
-      if match_node:id() == node:id() then
-        local start_row, start_col = node:start()
-        local _, end_col = node:end_()
-        local line = vim.api.nvim_buf_get_lines(M.current_buffer, start_row, start_row + 1, false)[1]
-        local line_len = string.len(line)
-        local capture_name = query.captures[id]
-
-        local command = "highlight @"
-        local color = fallback_fg
-        local color_raw = ""
-        while true do
-          local ok = pcall(function()
-            color_raw = vim.api.nvim_exec2(command .. capture_name, { output = true }).output:match("([^%s]+)$")
-          end)
-          if color_raw == nil then
-            break
-          end
-          if not ok then
-            break
-          end
-
-          if string.find(color_raw, "#") == nil then
-            -- print(color_raw)
-            command = "highlight "
-            capture_name = color_raw
-          else
-            color = color_raw:match("#%x+")
-            break
-          end
-        end
-
-        -- Calculates horizontal space
-        -- Formatting exists in this section as it simplifies
-        -- the design.
-        local diff_col = start_col - prev_end
-        prev_end = end_col
-        if diff_col < 0 then
-          diff_col = 0
-        end
-
-        if prev_row == nil then
-          prev_row = start_row - 1
-        end
-
-        if check_tabs then
-          check_tabs = false
-          diff_col = start_col
-        end
-        table.insert(
-          __line,
-          string.rep(" ", diff_col)
-            .. string.format(
-              "<span class='%s' style='color: %s'>%s</span>",
-              capture_name,
-              color,
-              html.escape_html(text)
-            )
-        )
-        ------
-
-        if line_len == end_col then
-          -- Calculates vertical space
-          local line_breaks = start_row - prev_row - 1
-          prev_row = start_row - 1
-
-          if line_breaks <= 0 then
-            line_breaks = 1
-          end
-
-          local l = table.concat(__line)
-          for _ = 1, line_breaks do
-            l = "\n" .. l
-          end
-          ------
-
-          check_tabs = true
-          line_number = line_number + 1
-          table.insert(__data, l)
-          __line = {}
-        end
-        return
-      end
-    end
+    local row1, col1, row2, col2 = node:range()
+    -- local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local links = vim.api.nvim_buf_get_extmarks(M.current_buffer, ns, { row1, col1 }, { row2, col2 }, {
+      overlap = true,
+    })
+    print(#links)
   end)
-  return table.concat(__data):gsub("^%s*(.-)%s*$", "%1")
+  -- print(links[1])
+
+  -- print(get_hl_group(M.current_buffer, node))
+  -- local text = vim.treesitter.get_node_text(node, M.current_buffer)
+  -- local start_row, start_col = node:start()
+  -- local end_row, end_col = node:end_()
+  -- marks[start_col] = { start_col, end_col, text, node, nil }
+  -- end)
+
+  -- for id, node in query:iter_captures(root, M.current_buffer, range[1] - 1, range[3]) do
+  --   local start_row, start_col = node:start()
+  --   local _, end_col = node:end_()
+  --   if marks[start_col] ~= nil then
+  --     print(marks[start_col][3])
+  --   end
+  -- end
+
+  -- for key, value in pairs(marks) do
+  --   for id, match_node in query:iter_captures(root, M.current_buffer) do
+  --     if match_node:id() == value[4]:id() then
+  --       marks[key][5] = query.captures[id]
+  --     end
+  --   end
+  -- end
+  --
+  -- for key, value in pairs(marks) do
+  --   print(key, value[2], value[3], value[4]:type(), value[4]:named(), find_color(value[5]))
+  -- end
+
+  -- return table.concat(__data):gsub("^%s*(.-)%s*$", "%1")
 end
 
 return M
--- plugin/snappy.lua
+++ plugin/snappy.lua
@@ -5,19 +5,19 @@ end
 
 vim.api.nvim_create_user_command("Snap", function()
   local value = generate()
-  if value ~= nil then
-    value = require("snappy.utils.html").generate_page(value)
-    local tmpfile = vim.fn.tempname() .. ".html"
-    vim.fn.writefile(vim.split(value, "\n"), tmpfile)
-
-    if vim.fn.has("mac") == 1 then
-      pcall(vim.cmd("!open " .. vim.fn.shellescape(tmpfile)))
-    elseif vim.fn.has("win32") == 1 then
-      pcall(vim.cmd("!start " .. vim.fn.shellescape(tmpfile)))
-    elseif vim.fn.has("unix") == 1 then
-      pcall(vim.cmd("!xdg-open " .. vim.fn.shellescape(tmpfile)))
-    end
-  end
+  -- if value ~= nil then
+  --   value = require("snappy.utils.html").generate_page(value)
+  --   local tmpfile = vim.fn.tempname() .. ".html"
+  --   vim.fn.writefile(vim.split(value, "\n"), tmpfile)
+  --
+  --   if vim.fn.has("mac") == 1 then
+  --     pcall(vim.cmd("!open " .. vim.fn.shellescape(tmpfile)))
+  --   elseif vim.fn.has("win32") == 1 then
+  --     pcall(vim.cmd("!start " .. vim.fn.shellescape(tmpfile)))
+  --   elseif vim.fn.has("unix") == 1 then
+  --     pcall(vim.cmd("!xdg-open " .. vim.fn.shellescape(tmpfile)))
+  --   end
+  -- end
 end, { desc = "Print visual selection range", range = true })
 
 -- vim.api.nvim_create_user_command("SnapI", function(opts)
